b75a2d5969fe8f05214302b17a7a7ba064551963
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2023-05-05 09:06:33 +0300

    add layuot header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,12 @@
-const App = () => <div className='App'>Фамильное дерево</div>;
+import Layout from './layout/Layout';
+import Home from './pages/home/Home';
+
+const App = () => (
+    <div className='App'>
+        <Layout>
+            <Home />
+        </Layout>
+    </div>
+);
 
 export default App;
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <div>Фамильное дерево</div>;
+};
+
+export default Home;
